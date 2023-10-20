93745fed8b2612d21f3379f48d640123f5345a22
akalyasri <akalya.s.sri@gmail.com>
2023-10-20 07:01:35 -0700

    basic input error framework in qtSPIM

--- fibonacciSequence.s
+++ fibonacciSequence.s
@@ -3,8 +3,8 @@
 #+---------------+--------------+-----------------------+---------------------------------------------------+---------------------------------------+
 #| 1             | print_int    | $a0 (4)               | Print the integer value stored in $a0.           | Integer value in $a0 is printed.     |
 #| 4             | print_str    | $a0 (4)               | Print the null-terminated string at $a0 address. | The string in memory at $a0 is printed. |
-#| 5             | read_int     | $v0 (2)               | Read an integer from standard input to $v0.      | The read integer is stored in $v0.    |
-#| 8             | read_str     | $a0 (4)               | Read a null-terminated string from input to $a0. | The read string is stored in $a0.    |
+#| 5             | read_int     | $v0 (2)               | Read an integer from standard input to $v0.      | The read integer is stored in $v0.                   |
+#| 8             | read_str     | $a0 (4)               | Read a null-terminated string from input to $a0. | The read string is stored in $a0.                    |
 #| 10            | exit         | $v0 (2)               | Terminate the program with exit code in $v0.     | Program execution ends with the specified exit code. |
 #+---------------+--------------+-----------------------+---------------------------------------------------+---------------------------------------+
 
@@ -16,10 +16,15 @@
 #   calculate fib num for n
 #   display result
 
+# the simulator doesnt support this //gnu assembler does
+#.equ result, $t0
+#.equ syscall, $v0
+
 .data
 
     prompt_msg: .asciiz "Enter a non-negative integer: "
     final_msg: .asciiz "Fibonacci number for input: "
+    error_msg: .asciiz "Error. Only enter non negative number. \n\n"
 
 
 .text
@@ -40,7 +45,7 @@
 
         # check if n is non negative
         bgez $t0, correctInput
-        j exit
+        j crashExit
 
     correctInput:
 
@@ -63,6 +68,8 @@
         #   PprevFbn = tempFbn;
         # }
 
+
+
     loop: 
 
 
@@ -81,5 +88,22 @@
         #exit
         li $v0, 10
         syscall
+
+
+    crashExit:
+
+        #print error msg
+        li $v0, 4
+        la $a0, error_msg
+        syscall
+
+        j main
+
+        #exit
+        #li $v0, 10
+        #syscall
+
+
+
 
         

be569694ac74ca27bf96a0377f1710285bd7ec73
akalyasri <akalya.s.sri@gmail.com>
2023-10-20 19:11:51 -0700

    wrote the steps of how the program should operate

--- binarySearch.s
+++ binarySearch.s
@@ -1,3 +1,23 @@
+#+---------------+--------------+-----------------------+---------------------------------------------------+-----------------------------------------------------+
+#| Syscall Number| Syscall Name | Input Argument Number| Description                                       | Output                                               |
+#+---------------+--------------+-----------------------+---------------------------------------------------+-----------------------------------------------------+
+#| 1             | print_int    | $a0 (4)               | Print the integer value stored in $a0.           | Integer value in $a0 is printed.                     |
+#| 4             | print_str    | $a0 (4)               | Print the null-terminated string at $a0 address. | The string in memory at $a0 is printed.              |
+#| 5             | read_int     | $v0 (2)               | Read an integer from standard input to $v0.      | The read integer is stored in $v0.                   |
+#| 8             | read_str     | $a0 (4)               | Read a null-terminated string from input to $a0. | The read string is stored in $a0.                    |
+#| 10            | exit         | $v0 (2)               | Terminate the program with exit code in $v0.     | Program execution ends with the specified exit code. |
+#+---------------+--------------+-----------------------+---------------------------------------------------+-----------------------------------------------------+
+
+#   steps:
+#   display prompt 
+#   get user to input 10 ascending numbers
+#   check the inputed numbers are in ascending order
+#   ask user to input 
+#   check if number is in list
+#   display result(and the index of the array)
+
+
+
 .data
 
 

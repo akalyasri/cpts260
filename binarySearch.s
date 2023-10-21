2fefd388e60d6df0b4ad470887104940a2441769
akalyasri <akalya.s.sri@gmail.com>
2023-10-21 06:47:11 -0700

    looks good, needs more testing

--- binarySearch.s
+++ binarySearch.s
@@ -19,35 +19,59 @@
 
 
 .data
-    prompt_msg: .asciiz "Enter 10 integers in ascending order: "
+    prompt_msg: .asciiz "Enter 10 integers in ascending order: \n\n"
+    input_msg: .asciiz "Enter an integer: " 
     target_msg: .asciiz "Enter your target integer: "
     error_msg: .asciiz "Error. Need to enter integers in ascending order. \n\n"
+    found_msg: .asciiz "Found at index: "
 
-    array: .space 40 # 40 each integer is 4 bits
+    #array: .space 40    #each integer is 4 bytes
+    array: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
 
 .text
     .globl main
 
         main:
 
-            #display prompt
+            # display prompt
             li $v0, 4
             la $a0, prompt_msg
             syscall
 
             # t0 is counter (i)
-            # t1 is length 10
-            # t2 is the current index
+            # t1 points to start of array 
+            # t2 is the length
             # t3 is mid
             # t4 is high
             # t5 is low
-            # t6 is the target
+            # t6 is target
+            # t7 is temp 
 
 
-            # get 10 integers from user
+          
+            li $t0, 0           #t0 is the counter
+            la $t1, array       #initializes starting address of array
+            
+        getUserInput:
 
+            li $v0, 4
+            la $a0, input_msg
+            syscall             #display input prompt
 
-            # check if the 10 given integers are in ascending order
+            li $v0, 5
+            syscall             #read integer
+
+            sw $v0, 0($t1)      #storing integer in array
+            
+            addi $t0, $t0, 1    #icrement counter
+            addi $t1, $t1, 4    #increment array pointer
+
+            li $t2, 10          #set t2 as length 10
+
+            beq $t0, $t2, getTarget
+            j getUserInput
+
+        getTarget:
 
 
             # get target value from user
@@ -59,17 +83,58 @@
             syscall
             move $t6, $v0   # t6 is the target
 
-            # perform binary search for given target
+            li $t5, 0
+            li $t4, 9
+            li $t7, 0
+
+        binarySearchLoop:
+
+            # t0 is counter (i)
+            # t1 points to start of array 
+            # t2 is the length
+            # t3 is mid
+            # t4 is high
+            # t5 is low
+            # t6 is target
+            # t7 is temp 
 
             
 
+            add $t3, $t5, $t4       # mid = low + high
+            srl $t3, $t3, 1         # mid = mid / 2
 
-            # display if target was hit and if so, what index it was found at
+            
+            sll $t7, $t3, 2         #getting offset
+            lw $t7, array($t7)      #load mid element
 
+            beq $t7, $t6, found        #if target = array[mid] , found 
+            bge $t5, $t4, exit         #if low >= high, exit
 
+            bgt $t7, $t6, searchHigh    #if array[mid] > target, search high
+            j searchLow                 #else array[mid] < target, searchlow
 
-        getArrayInput:
-            li $v0, 5
+        
+        searchHigh:
+
+            addi $t4, $t3, -1
+            j binarySearchLoop
+
+        searchLow: 
+            
+            addi $t5, $t3, 1
+            j binarySearchLoop 
+
+            
+        found:
+
+            li $v0, 4
+            la $a0, found_msg
+            syscall
+
+            li $v0, 1
+            #addi $t3, $t3, -3
+            move $a0, $t3
+            syscall
 
 
 
@@ -79,5 +144,4 @@
             li $v0, 10
             syscall
             
-
 
